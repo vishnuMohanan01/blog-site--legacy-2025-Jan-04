68a1c5ac8d5e162546f8905e7ac75aac3bbc4ccb
vishnuMohonan01 <iamvm.dev@gmail.com>
2024-01-14 12:24:25 +0530

    Cleans up post layout

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -33,8 +33,8 @@ export default function PostLayout({ content, children }: LayoutProps) {
       <article>
         <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
           <header className="pt-6 xl:pb-6">
-            <div className="space-y-1 text-center">
-              <dl className="space-y-10">
+            <div className="space-y-1">
+            <dl className="space-y-10">
                 <div>
                   <dt className="sr-only">Published on</dt>
                   <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
@@ -49,16 +49,14 @@ export default function PostLayout({ content, children }: LayoutProps) {
               </div>
             </div>
           </header>
-          <div className="divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0">
-            <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
-              <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
-              <div className="pb-6 pt-6 text-sm text-gray-700 dark:text-gray-300">
-                <Link href={discussUrl(path)} rel="nofollow">
-                  Discuss on X
-                </Link>
-                {` • `}
-                <Link href={editUrl(filePath)}>View on GitHub</Link>
-              </div>
+          <div className="py-8 divide-y divide-gray-200 dark:divide-gray-700">
+            <div className="max-w-none prose dark:prose-invert pt-6 pb-12">{children}</div>
+            <div className="py-4 text-gray-500">
+              <Link className="hover:underline underline-offset-2" href={discussUrl(path)} rel="nofollow">
+                Discuss on X
+              </Link>
+              {` • `}
+              <Link className="hover:underline underline-offset-2" href={editUrl(filePath)}>View on GitHub</Link>
             </div>
           </div>
         </div>
