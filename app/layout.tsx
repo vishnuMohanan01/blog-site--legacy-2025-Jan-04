a15e62adcf0f2115f202373893c1c07739a012b6
vishnuMohonan01 <iamvm.dev@gmail.com>
2024-01-14 14:28:41 +0530

    Fixes build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
 
-import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
+import { Analytics as VercelAnalytics } from '@vercel/analytics/react'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
